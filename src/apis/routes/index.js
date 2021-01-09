8a2ec352f39c17db8e537f35b9394404cad453cf
kkoon9 <eric@Ericui-MacBookPro.local>
2021-01-09 18:57:36 +0900

    style: 쓸모없는 부분 삭제

--- src/apis/routes/index.js
+++ src/apis/routes/index.js
@@ -2,17 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-/**
- * @api {get} /user/:id API Docs 예시
- * @apiName GetUser
- * @apiGroup User
- *
- * @apiParam {Number} id Users unique ID.
- *
- * @apiSuccess {String} firstname Firstname of the User.
- * @apiSuccess {String} lastname  Lastname of the User.
- */
-
 router.use('/cherish', require('./cherish'));
 
 router.use('/water', require('./water'));
