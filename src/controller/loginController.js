efd695c927c96b2d55ae35adcd4945eaa19dcb1d
kkoon9 <eric@Ericui-MacBookPro.local>
2021-01-10 16:44:40 +0900

    리팩토링 : 로그인 API

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,12 +1,7 @@
-const {
-  validationResult
-} = require('express-validator');
+const { validationResult } = require('express-validator');
 const dayjs = require('dayjs');
 
-const {
-  User,
-  sequelize
-} = require('../models');
+const { User, sequelize } = require('../models');
 const ut = require('../modules/util');
 const sc = require('../modules/statusCode');
 const rm = require('../modules/responseMessage');
@@ -14,25 +9,24 @@ const userService = require('../service/userService');
 
 module.exports = {
   signin: async (req, res) => {
-    // 1. req.body에서 데이터 가져오기
-    const {
-      email,
-      password
-    } = req.body;
-    //2. request data 확인하기, email, password data가 없다면 NullValue 반환
-    if (!email || !password) {
-      return res.status(sc.BAD_REQUEST).send(ut.fail(rm.NULL_VALUE));
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
     }
+    // 1. req.body에서 데이터 가져오기
+    const { email, password } = req.body;
 
     try {
-      //3. 존재하는 아이디인지 확인하기. 존재하지 않는 아이디면 NO USER 반환
+      //2. 존재하는 아이디인지 확인하기. 존재하지 않는 아이디면 NO USER 반환
       const alreadyEmail = await userService.emailCheck({
         email,
       });
       if (!alreadyEmail) {
         return res.status(sc.BAD_REQUEST).send(ut.fail(rm.NO_USER));
       }
-      //4. password(=alreadyPassword)와 일치하면 true, 일치하지 않으면 Miss Match password 반환
+      //3. password(=alreadyPassword)와 일치하면 true, 일치하지 않으면 Miss Match password 반환
       if (password !== alreadyEmail.password) {
         return res.status(sc.BAD_REQUEST).send(ut.fail(rm.MISS_MATCH_PW, password));
       }
@@ -40,21 +34,16 @@ module.exports = {
         email,
         password,
       });
-      //5. status: 200 ,message: SIGN_IN_SUCCESS, data: email반환
-      return res.status(sc.OK).send(ut.success(rm.SIGN_IN_SUCCESS, user.id));
+      const UserId = user.id;
+      //4. status: 200 ,message: SIGN_IN_SUCCESS, data: email반환
+      return res.status(sc.OK).send(ut.success(rm.SIGN_IN_SUCCESS, { UserId }));
     } catch (error) {
       return res.status(sc.INTERNAL_SERVER_ERROR).send(ut.fail(rm.SIGN_IN_FAIL));
     }
   },
 
   signup: async (req, res) => {
-    const {
-      email,
-      password,
-      sex,
-      birth,
-      nickname
-    } = req.body;
+    const { email, password, sex, birth, nickname } = req.body;
 
     if (!email || !password || !sex || !birth || !nickname) {
       console.log('필요한 값이 없습니다!');
@@ -62,7 +51,7 @@ module.exports = {
     }
     try {
       const alreadyEmail = await userService.emailCheck({
-        email
+        email,
       });
       if (alreadyEmail) {
         console.log('이미 존재하는 이메일 입니다.');
@@ -70,9 +59,11 @@ module.exports = {
       }
       const user = await userService.signup(email, password, sex, birth, nickname);
 
-      return res.status(sc.OK).send(ut.success(rm.SIGN_UP_SUCCESS, {
-        nickname: user.nickname,
-      }));
+      return res.status(sc.OK).send(
+        ut.success(rm.SIGN_UP_SUCCESS, {
+          nickname: user.nickname,
+        })
+      );
     } catch (error) {
       console.error(error);
       return res.status(sc.INTERNAL_SERVER_ERROR).send(ut.fail(rm.SIGN_UP_FAIL));
