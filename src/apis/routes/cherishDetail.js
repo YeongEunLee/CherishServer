d04e3ec24b2bfc03c4b007b721cc48f044432f47
YeongEunLee <zduddmszz@gmail.com>
2021-02-14 23:06:02 +0900

    CherishDetail API 수정

--- src/apis/routes/cherishDetail.js
+++ src/apis/routes/cherishDetail.js
@@ -3,14 +3,14 @@ const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
 
-const userDetailController = require('../../controller/userDetailController');
+const cherishDetailController = require('../../controller/cherishDetailController');
 
 /**
- * @api {get}/getUserDetail/:id
- * @apiName getUserDetail
- * @apiGroup getUserDetail
+ * @api {get}/getCherishDetail/:id
+ * @apiName getCherishDetail
+ * @apiGroup getCherishDetail
  */
 //router.get('/', [check('id', 'id is required').not().isEmpty()], userDetailController.getUserDetail);
-router.get('/:CherishId', userDetailController.getUserDetail);
+router.get('/:CherishId', cherishDetailController.getCherishDetail);
 
 module.exports = router;
