825ce1fc4c229c6b03a577252370772e3b6d5657
kkoon9 <rndrnjs2003@gmail.com>
2021-02-20 21:52:17 +0900

    feat: 번호 인증 API

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,11 +1,12 @@
-const {
-  User
-} = require('../models');
+const crypto = require('crypto');
+const request = require('request');
+
+const { User } = require('../models');
+
+const secretKey = require('../config');
 
 module.exports = {
-  emailCheck: async ({
-    email
-  }) => {
+  emailCheck: async ({ email }) => {
     try {
       const alreadyEmail = await User.findOne({
         where: {
@@ -19,10 +20,7 @@ module.exports = {
     }
   },
 
-  signin: async ({
-    email,
-    password
-  }) => {
+  signin: async ({ email, password }) => {
     try {
       const user = await User.findOne({
         where: {
@@ -49,12 +47,83 @@ module.exports = {
         nickname,
         salt: '4321234',
         phone,
-        birth
+        birth,
       });
       return user;
     } catch (err) {
       console.log(err);
       throw err;
+    }
+  },
+
+  /**
+   * 인증 번호 보내기
+   * @summary 핸드폰 인증을 위해 6자리 번호를 무작위로 보낸다.
+   * @param 핸드폰 번호
+   * @return 인증번호(6자리)
+   */
+  sendNumber: async ({ phone }) => {
+    const NCP_accessKey = secretKey.NCP_API_access_key;
+    const NCP_secretKey = secretKey.NCP_API_secret_key;
+    const NCP_serviceID = secretKey.SENS_service_ID;
+    const myPhoneNumber = secretKey.myPhoneNumber;
+    console.log(NCP_accessKey);
+    console.log(NCP_secretKey);
+    console.log(NCP_serviceID);
+    console.log(myPhoneNumber);
+    const space = ' '; // one space
+    const newLine = '\n'; // new line
+    const method = 'POST'; // method
+    const url = `https://sens.apigw.ntruss.com/sms/v2/services/${NCP_serviceID}/messages`;
+    // url (include query string)
+    const url2 = `/sms/v2/services/${NCP_serviceID}/messages`;
+    const timestamp = Date.now().toString(); // current timestamp (epoch)
+    let message = [];
+    let hmac = crypto.createHmac('sha256', NCP_secretKey);
+    message.push(method);
+    message.push(space);
+    message.push(url2);
+    message.push(newLine);
+    message.push(timestamp);
+    message.push(newLine);
+    message.push(NCP_accessKey);
+    const signature = hmac.update(message.join('')).digest('base64');
+    const verifyCode = Math.floor(Math.random() * (9999 - 1000)) + 1000;
+    try {
+      request(
+        {
+          method: method,
+          json: true,
+          uri: url,
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'x-ncp-iam-access-key': NCP_accessKey,
+            'x-ncp-apigw-timestamp': timestamp,
+            'x-ncp-apigw-signature-v2': signature.toString(),
+          },
+          body: {
+            type: 'SMS',
+            contentType: 'COMM',
+            countryCode: '82',
+            subject: '인증 알림',
+            from: myPhoneNumber,
+            content: `체리쉬 인증번호 ${verifyCode}입니다.`,
+            messages: [
+              {
+                to: phone,
+              },
+            ],
+          },
+        },
+        function (err, res, html) {
+          if (err) console.log('err : ', err);
+          console.log('html : ', html);
+        }
+      );
+      return verifyCode;
+    } catch (err) {
+      console.log(err);
+      return 0;
     }
   },
 };
